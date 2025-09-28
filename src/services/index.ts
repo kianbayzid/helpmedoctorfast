5a3e814c4999ba40491d0affd44a49058b5c8632
Kian Bayzid <134166704+kianbayzid@users.noreply.github.com>
2025-09-28 06:09:57 -0400

    1st

--- src/services/index.ts
+++
@@ -1,9 +0,0 @@
-// Export all services
-export { doctorService } from './doctorService';
-export { patientService } from './patientService';
-export { messageService } from './messageService';
-export { huggingfaceService } from './huggingfaceService';
-export { apiClient } from './api';
-
-// Export types
-export * from '../types';
